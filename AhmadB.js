69a31adf3c7881e53870d1ea122baf61bd094f37
ahmad <ahmadheeba10@gmail.com>
2024-05-02 21:00:18 +0300

    done

---
+++ AhmadB.js
@@ -0,0 +1,12 @@
+
+title = document.getElementById('title');
+price = document.getElementById('price');
+taxes = document.getElementById('taxes');
+ads = document.getElementById('ads');
+discount = document.getElementById('discount');
+total = document.getElementById('total');
+count = document.getElementById('count');
+category = document.getElementById('category');
+
+
+
